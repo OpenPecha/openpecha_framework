2e5101c64481b1e5d5ef1323b35c7b6dc7dd144d
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-09-25 16:32:22 +0530

    test for function as pipe

--- tests/pipeline/simple_pipes/test_simple_pipes.py
+++ tests/pipeline/simple_pipes/test_simple_pipes.py
@@ -1,8 +1,32 @@
 from pathlib import Path
 
-from stanza_extractor import SpaceNormalizer, Stanza  # noqa
+from pecha_framework.pipeline import Document, Pipe, Pipeline
 
-from pecha_framework.pipeline import Document, Pipeline
+
+class SpaceNormalizer(Pipe):
+    name = "space_normalizer"
+
+    def __call__(self, doc: Document):
+        """Normalize space at start or end of sentence in the document."""
+        doc.text = "\n".join(line.strip() for line in doc.text.split("\n"))
+        return doc
+
+
+class Stanza(Pipe):
+    name = "stanza_extractor"
+
+    def __call__(self, doc: Document):
+        """
+        Extract Stanza from the doc.text by two newlines.
+        """
+        char_count = 0
+        stanza_anns = []
+        for stanza in doc.text.split("\n\n"):
+            stanza_anns.append({"start": char_count, "end": char_count + len(stanza)})
+            char_count += len(stanza) + 2
+        doc.annotations["stanza"] = stanza_anns
+        return doc
+
 
 expected_stanzas_output = [
     "The night was still, the moon aglow,\nA silver thread on earth below.\nThe stars, like diamonds, softly gleamed,\nWhile all the world in silence dreamed.",  # noqa
@@ -17,6 +41,8 @@ def test_simple_pipes():
     doc = Document(text=english_poem)
     pipeline = Pipeline(components=["space_normalizer", "stanza_extractor"])
     doc = pipeline(doc)
+
+    """ Test the text attribute """
 
     stanza_anns = doc.annotations["stanza"]
     assert len(stanza_anns) == 2
